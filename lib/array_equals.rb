5541b623db882d7a6815a5e55f6dd3e5c6908663
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-03-09 23:30:50 -0700

    refactored the code and got all the tests passed

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,26 +1,23 @@
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
-def array_equals(array1, array2)
-  return true if array1 ==  nil && array2 == nil || array1 == [] && array2 == []
-  temp = []
-  index = 0
-  if array1 != nil && array2 != nil && array1 != [] && array2 != [] && array1.length == array2.length 
-    while index > array1.length
-      if array1[index] == array2[index]
-        temp << true
-      end
-      index += 1
-    end
-    if temp.length == array1.length
-      return true
-    else
-      return false
-    end
-  end
-  return false
-end
- # if the arrays contan the same count of elements 
-      #&& the element values in the array are the same
-      #&&they are the in same exact order
+# Time complexity = O(n) - it depends on the array length
+# Space complexity = O(1) - it doesn't create any new array
 
-    # else return false
+def array_equals(array1, array2)
+  return true if (array1 == nil && array2 == nil) || (array1 == [] && array2 == [])
+  return false if (array1 == nil && array2 != nil) || (array1 != nil && array2 == nil) 
+  return false if (array1 != nil && array2 != nil) && (array1.length != array2.length)
+
+  if (array1 != nil && array2 != nil) && (array1.length == array2.length)
+    index = 0
+    while index < array1.length
+      if array1[index] != array2[index]
+        return false
+      end
+      index +=1
+    end
+    return true  
+  end
+  return false  
+end
+ 
