621961bbbf80fcd18f1d372e6d275891e02585fc
saraleecodes <sara.lee.higgins@gmail.com>
2020-03-11 15:56:58 -0700

    passes all tests

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -3,11 +3,14 @@
 def array_equals(array1, array2)
   return true if array1 == nil && array2 == nil
   return true if array1 == [] && array2 == []
+  return false if array1 == nil || array2 == nil
   
   unless array1.length != array2.length
     array1.length.times do |index|
       return false if array1[index] != array2[index]
     end
+
+    return true
   end
   
   return false
