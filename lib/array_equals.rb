1cdd6a4b461fe7334218ccc5a969ddaa5b6fd6c9
saraleecodes <sara.lee.higgins@gmail.com>
2020-03-11 15:50:47 -0700

    passes all tests except 0001, 0005, and 0006

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -2,5 +2,13 @@
 # and the same integer values in the same exact order
 def array_equals(array1, array2)
   return true if array1 == nil && array2 == nil
+  return true if array1 == [] && array2 == []
+  
+  unless array1.length != array2.length
+    array1.length.times do |index|
+      return false if array1[index] != array2[index]
+    end
+  end
+  
   return false
 end
