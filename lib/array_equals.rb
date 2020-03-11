6ea5ad071a07b64329dacb7064840b82c5d8aa58
saraleecodes <sara.lee.higgins@gmail.com>
2020-03-11 15:37:23 -0700

    checks if both arrays are equal to nil

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,5 +1,6 @@
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
 def array_equals(array1, array2)
-  raise NotImplementedError
+  return true if array1 == nil && array2 == nil
+  return false
 end
