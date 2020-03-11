e1b5a0c15aeabb037a04ada881acf0b359b4d23c
Faezeh Ashtiani <faa@Faezehs-MacBook-Pro.local>
2020-03-10 21:55:19 -0700

    adds the method and updates the test file for not getting warnings

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,5 +1,25 @@
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
 def array_equals(array1, array2)
-  raise NotImplementedError
+  # raise NotImplementedError
+  if !(array1.nil?) && !(array2.nil?)
+    difference_count = 0
+    if array1.length == array2.length
+      array1.length.times do |i|
+        difference_count += 1 if array1[i] != array2[i]
+      end
+      if difference_count != 0 
+        return false
+      end
+    else
+      return false
+    end
+  elsif array1.nil? && !(array2.nil?)
+    return false
+  elsif array2.nil? && !(array1.nil?)
+    return false
+  end
+
+  return true
 end
+
